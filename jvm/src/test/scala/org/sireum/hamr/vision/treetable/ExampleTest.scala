10befc84ecd61acb6b1981e5e00138b223f40676
Jason Belt <belt@ksu.edu>
2023-09-19 11:58:26 -0500

    only run gui tests when inside IVE

--- jvm/src/test/scala/org/sireum/hamr/vision/treetable/ExampleTest.scala
+++ jvm/src/test/scala/org/sireum/hamr/vision/treetable/ExampleTest.scala
@@ -11,87 +11,87 @@ import javax.swing.{JFrame, JScrollPane}
 
 class ExampleTest extends TestSuite {
 
-  * - {
-    assert(T)
-  }
+  val isCI: B = Os.env("GITLAB_CI").nonEmpty || Os.env("GITHUB_ACTIONS").nonEmpty || Os.env("BUILD_ID").nonEmpty
 
   "Live Feed" in {
-    val seedGen: Gen64 = Random.Gen64Impl(Xoshiro256.create)
+    if (!isCI && Os.env("__CFBundleIdentifier") == Some(string"com.jetbrains.intellij.ce")) {
+      val seedGen: Gen64 = Random.Gen64Impl(Xoshiro256.create)
 
-    val freshRandomLib: RandomLib = {
-      val r = RandomLib(Random.Gen64Impl(Xoshiro256.createSeed(seedGen.genU64())))
+      val freshRandomLib: RandomLib = {
+        val r = RandomLib(Random.Gen64Impl(Xoshiro256.createSeed(seedGen.genU64())))
 
-      // TODO: should slangcheck reject Value's children who introduce recursion?
-      //       issue is their next methods will likely lead to stack overflows
+        // TODO: should slangcheck reject Value's children who introduce recursion?
+        //       issue is their next methods will likely lead to stack overflows
 
-      // TODO: shouldn't there be a config for strings so that we can set
-      //       the max number of characters to generate?
-      r.set_numElement(10)
+        // TODO: shouldn't there be a config for strings so that we can set
+        //       the max number of characters to generate?
+        r.set_numElement(10)
 
-      // TODO: seems like nextC could have a from/between.  This issue with filters
-      //       is small bounds leads to failures to gen a random c that satisfies
-      //       the bound
-      r.set_Config_C(r.get_Config_C()(filter = c => c.value >= 32 && c.value <= 55639))
-    }
-
-    def nextString(): String = {
-      while (true) {
-        try {
-          return freshRandomLib.nextString()
-        } catch {
-          case e: Throwable =>
-        }
+        // TODO: seems like nextC could have a from/between.  This issue with filters
+        //       is small bounds leads to failures to gen a random c that satisfies
+        //       the bound
+        r.set_Config_C(r.get_Config_C()(filter = c => c.value >= 32 && c.value <= 55639))
       }
-      halt("Infeasible")
-    }
 
-    val (model, elems): (DemoTreeTableModelSC, ISZ[compSC]) = {
-      val input = Array[InputSC](
-        new InputSC(Array[Object]("In 0", "", "This is an input port")),
-        new InputSC(Array[Object]("In 1", "", "I am another port")))
-      val output = Array[OutputSC](
-        new OutputSC(Array[Object]("Out 0", "", "This is an Output")),
-        new OutputSC(Array[Object]("Out 1", "", "Haha robot talk")))
-      val component = new compSC(new InputsSC(input), new OutputsSC(output), "Component 0")
-      val input2 = Array[InputSC](
-        new InputSC(Array[Object]("In 0", "", "More inputs!")),
-        new InputSC(Array[Object]("In 1", "", "The bite of '87?")))
-      val output2 = Array[OutputSC](
-        new OutputSC(Array[Object]("Out 0", "", "Bendy?")),
-        new OutputSC(Array[Object]("Out 1", "", "Pizza orders")))
-      val component2 = new compSC(new InputsSC(input2), new OutputsSC(output2), "Component 1")
+      def nextString(): String = {
+        while (true) {
+          try {
+            return freshRandomLib.nextString()
+          } catch {
+            case e: Throwable =>
+          }
+        }
+        halt("Infeasible")
+      }
 
-      (new DemoTreeTableModelSC(Array[compSC](component, component2)), ISZ(component, component2))
-    }
+      val (model, elems): (DemoTreeTableModelSC, ISZ[compSC]) = {
+        val input = Array[InputSC](
+          new InputSC(Array[Object]("In 0", "", "This is an input port")),
+          new InputSC(Array[Object]("In 1", "", "I am another port")))
+        val output = Array[OutputSC](
+          new OutputSC(Array[Object]("Out 0", "", "This is an Output")),
+          new OutputSC(Array[Object]("Out 1", "", "Haha robot talk")))
+        val component = new compSC(new InputsSC(input), new OutputsSC(output), "Component 0")
+        val input2 = Array[InputSC](
+          new InputSC(Array[Object]("In 0", "", "More inputs!")),
+          new InputSC(Array[Object]("In 1", "", "The bite of '87?")))
+        val output2 = Array[OutputSC](
+          new OutputSC(Array[Object]("Out 0", "", "Bendy?")),
+          new OutputSC(Array[Object]("Out 1", "", "Pizza orders")))
+        val component2 = new compSC(new InputsSC(input2), new OutputsSC(output2), "Component 1")
 
-    val tt = new JTreeTableSC(model)
-    val jf = new JFrame()
-    val jsp = new JScrollPane(tt)
-    jf.add(jsp, BorderLayout.CENTER)
-    jf.pack()
-    jf.setVisible(true)
+        (new DemoTreeTableModelSC(Array[compSC](component, component2)), ISZ(component, component2))
+      }
 
-    while (true) {
-      for (j <- 0 to 1) {
-        val component = elems(j)
+      val tt = new JTreeTableSC(model)
+      val jf = new JFrame()
+      val jsp = new JScrollPane(tt)
+      jf.add(jsp, BorderLayout.CENTER)
+      jf.pack()
+      jf.setVisible(true)
 
-        val in1 = nextString().native
-        val in2 = nextString().native
+      while (true) {
+        for (j <- 0 to 1) {
+          val component = elems(j)
 
-        tt.updatePort(component.getIn.inputs, s"In 0", in1, "")
-        tt.updatePort(component.getIn.inputs, s"In 1", in2, "")
+          val in1 = nextString().native
+          val in2 = nextString().native
 
-        // let component think
-        Thread.sleep(500)
+          tt.updatePort(component.getIn.inputs, s"In 0", in1, "")
+          tt.updatePort(component.getIn.inputs, s"In 1", in2, "")
 
-        val out1 = nextString().native
-        val out2 = nextString().native
+          // let component think
+          Thread.sleep(500)
 
-        tt.updatePort(component.getOut.outputs, s"Out 0", out1, "")
-        tt.updatePort(component.getOut.outputs, s"Out 1", out2, "")
+          val out1 = nextString().native
+          val out2 = nextString().native
 
-        // wait before switching to the other component
-        Thread.sleep(2000)
+          tt.updatePort(component.getOut.outputs, s"Out 0", out1, "")
+          tt.updatePort(component.getOut.outputs, s"Out 1", out2, "")
+
+          // wait before switching to the other component
+          Thread.sleep(2000)
+        }
       }
     }
   }
